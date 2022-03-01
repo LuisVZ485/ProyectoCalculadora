47a489caf5263ef6766065dcbefe65474ff3bc0a
luis <juggerlavz23gmail.com>
2022-02-28 19:01:43 -0500

    developing: operations with buttons

--- ProyectoCalculadora/FormCalculadora.Designer.cs
+++ ProyectoCalculadora/FormCalculadora.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ProyectoCalculadora
 {
-    partial class Form1
+    partial class FormCalculadora
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -193,6 +193,7 @@ namespace ProyectoCalculadora
             this.btnPunto.TabIndex = 12;
             this.btnPunto.Text = ".";
             this.btnPunto.UseVisualStyleBackColor = true;
+            this.btnPunto.Click += new System.EventHandler(this.btnPunto_Click);
             // 
             // btnMultiplicacion
             // 
@@ -203,6 +204,7 @@ namespace ProyectoCalculadora
             this.btnMultiplicacion.TabIndex = 13;
             this.btnMultiplicacion.Text = "X";
             this.btnMultiplicacion.UseVisualStyleBackColor = true;
+            this.btnMultiplicacion.Click += new System.EventHandler(this.btnMultiplicacion_Click);
             // 
             // btnResta
             // 
@@ -213,6 +215,7 @@ namespace ProyectoCalculadora
             this.btnResta.TabIndex = 14;
             this.btnResta.Text = "-";
             this.btnResta.UseVisualStyleBackColor = true;
+            this.btnResta.Click += new System.EventHandler(this.btnResta_Click);
             // 
             // btnMas
             // 
@@ -246,6 +249,7 @@ namespace ProyectoCalculadora
             this.btnModulo.TabIndex = 17;
             this.btnModulo.Text = "%";
             this.btnModulo.UseVisualStyleBackColor = true;
+            this.btnModulo.Click += new System.EventHandler(this.btnModulo_Click);
             // 
             // btnDivision
             // 
@@ -256,6 +260,7 @@ namespace ProyectoCalculadora
             this.btnDivision.TabIndex = 18;
             this.btnDivision.Text = "/";
             this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
             // 
             // btnReset
             // 
@@ -266,6 +271,7 @@ namespace ProyectoCalculadora
             this.btnReset.TabIndex = 19;
             this.btnReset.Text = "C";
             this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // btnBorrar
             // 
@@ -276,8 +282,9 @@ namespace ProyectoCalculadora
             this.btnBorrar.TabIndex = 20;
             this.btnBorrar.Text = "<";
             this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
             // 
-            // Form1
+            // FormCalculadora
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -304,7 +311,7 @@ namespace ProyectoCalculadora
             this.Controls.Add(this.btnUno);
             this.Controls.Add(this.lblHistorial);
             this.Controls.Add(this.cajaResultado);
-            this.Name = "Form1";
+            this.Name = "FormCalculadora";
             this.Text = "Calculadora";
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ ProyectoCalculadora/FormCalculadora.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoCalculadora
+{
+    public enum Operacion
+    {
+        NoDefinida = 0,
+        Suma = 1,
+        Resta = 2,
+        Division = 3,
+        Multiplicacion= 4,
+        Modulo = 5
+    }
+    public partial class FormCalculadora : Form
+    {
+        private double valor1 = 0;
+        private double valor2 = 0;
+        Operacion operador = Operacion.NoDefinida;
+        bool numeroLeido = false;
+
+        private double EjecutarOperacion()
+        {
+            double resultado = 0;
+
+            switch (operador)
+            {
+                case Operacion.NoDefinida:
+                    break;
+                case Operacion.Suma:
+                    resultado = valor1 + valor2;
+                    break;
+                case Operacion.Resta:
+                    resultado = valor1 - valor2;
+                    break;
+                case Operacion.Division:
+                    if (valor2 == 0)
+                    {
+                        MessageBox.Show("No se puede dividir entre 0");
+                        resultado = 0;
+                    }
+                    else
+                    {
+                        resultado = valor1 / valor2;
+                    }
+                    break;
+                case Operacion.Multiplicacion:
+                    resultado = valor1 * valor2;
+                    break;
+                case Operacion.Modulo:
+                    resultado = valor1 % valor2;
+                    break;
+            }
+            return resultado;
+        }
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void LeerNumero(string numero)
+        {
+            numeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = numero;
+            }
+            else
+            {
+                cajaResultado.Text += numero;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void btnCero_Click(object sender, EventArgs e)
+        {
+            numeroLeido = true;
+            if (cajaResultado.Text == "0")
+            {
+                return;
+            }
+            else if (cajaResultado.Text != "0")
+            {
+                cajaResultado.Text += "0";
+            }
+            else
+            {
+                cajaResultado.Text = "0";
+            }
+            
+        }
+
+        private void btnUno_Click(object sender, EventArgs e)
+        {
+            LeerNumero("1");
+        }
+
+        private void btnDos_Click(object sender, EventArgs e)
+        {
+            LeerNumero("2");
+        }
+
+        private void btnTres_Click(object sender, EventArgs e)
+        {
+            LeerNumero("3");
+        }
+
+        private void btnCuatro_Click(object sender, EventArgs e)
+        {
+            LeerNumero("4");
+        }
+
+        private void btnCinco_Click(object sender, EventArgs e)
+        {
+            LeerNumero("5");
+        }
+
+        private void btnSeis_Click(object sender, EventArgs e)
+        {
+            LeerNumero("6");
+        }
+
+        private void btnSiete_Click(object sender, EventArgs e)
+        {
+            LeerNumero("7");
+        }
+
+        private void btnOcho_Click(object sender, EventArgs e)
+        {
+            LeerNumero("8");
+        }
+
+        private void btnNueve_Click(object sender, EventArgs e)
+        {
+            LeerNumero("9");
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            if (valor2 == 0 && numeroLeido)
+            {
+                valor2 = Convert.ToDouble(cajaResultado.Text);
+                lblHistorial.Text += valor2 + " = ";
+                double resultado = EjecutarOperacion();
+                valor1 = 0;
+                valor2 = 0;
+                numeroLeido = false;
+                cajaResultado.Text = Convert.ToString(resultado);
+            }
+        }
+
+        private void ObtenerValor(string signo)
+        {
+            valor1 = Convert.ToDouble(cajaResultado.Text);
+            lblHistorial.Text = cajaResultado.Text + signo;
+            cajaResultado.Text = "0";
+        }
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Suma;
+            ObtenerValor(" + ");
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Resta;
+            ObtenerValor(" - ");
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Multiplicacion;
+            ObtenerValor(" * ");
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Division;
+            ObtenerValor(" / ");
+        }
+
+        private void btnModulo_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Modulo;
+            ObtenerValor(" % ");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = "0";
+            lblHistorial.Text = "";
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            string txtResultado = cajaResultado.Text;
+            txtResultado = txtResultado.Substring(0, txtResultado.Length - 1);
+
+
+            if (txtResultado.Length == 1 && txtResultado.Contains("-"))
+            {
+                cajaResultado.Text = "0";
+            }
+            else
+            {
+                cajaResultado.Text = txtResultado;
+            }
+        }
+
+        private void btnPunto_Click(object sender, EventArgs e)
+        {
+            if (cajaResultado.Text.Contains("."))
+            {
+                return;
+            }
+            else
+            {
+                cajaResultado.Text += ".";
+            }
+        }
+    }
+}
